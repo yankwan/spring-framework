821f44006e0675fe52ba421e050f4c23e69afa7c
liuyanjun <yanjliu@126.com>
2019-05-22 15:25:24 +0800

    edit

--- mytest/src/main/java/com/spring/demo/helloworld/MainApp.java
+++ mytest/src/main/java/com/spring/demo/helloworld/MainApp.java
@@ -8,15 +8,15 @@ import org.springframework.core.io.ClassPathResource;
 
 public class MainApp {
 	public static void main(String[] args) {
-//		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-//		HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
-//		obj.getMessage();
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+		obj.getMessage();
 
-		ClassPathResource resource = new ClassPathResource("beans.xml");
-		DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
-		// BeanDefinitionReader 读取并解析Resource资源
-		XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory);
-		// 将资源封装成BeanDefinition
-		reader.loadBeanDefinitions(resource);
+//		ClassPathResource resource = new ClassPathResource("beans.xml");
+//		DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
+//		// BeanDefinitionReader 读取并解析Resource资源
+//		XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory);
+//		// 将资源封装成BeanDefinition
+//		reader.loadBeanDefinitions(resource);
 	}
 }
